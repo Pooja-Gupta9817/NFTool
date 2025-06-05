ee11a7599175a8723ee7b11a07a96ed2f09279ba
Pooja-Gupta9718 <pg98171@gmail.com>
2025-06-05 16:01:28 +0530

    Initial commits

---
+++ App.Core/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopTool.App.Core
+{
+    //This allows reusable data access for any entity class (like User, Product, etc.)
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+
+}
---
+++ App.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using DesktopTool.App.Core;
+using DesktopTool.App.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopTool.App.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<Data.ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString)); // This connects to Azure SQL
+
+            services.AddScoped<Core.IUserRepository, Data.UserRepository>();
+
+            return services;
+        }
+    }
+
+}
